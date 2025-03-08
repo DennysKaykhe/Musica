d7eab2d76549b8b34d0d433661f37a45eebd4ebb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-07 21:48:06 -0300

    Umaa música fofo pro seu grande coração professor s2

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿Console.Write(@"[Letra de ""Meu Abrigo"" com Melim]
+
+[Intro]
+Uh, uh, uh, uh
+Uh, uh
+
+[Verso 1]
+Desejo a você
+O que há de melhor
+A minha companhia
+Pra não se sentir só
+O Sol, a Lua e o mar
+Passagem pra viajar
+Pra gente se perder
+E se encontrar
+
+[Pré-Refrão]
+Vida boa, brisa e paz
+Nossas brincadeiras ao entardecer
+Rir à toa é bom demais
+O meu melhor lugar sempre é você
+
+");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("[Refrão]");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"Você é a razão da minha felicidade
+Não vá dizer que eu não sou sua cara-metade
+Meu amor, por favor, vem viver comigo
+No seu colo é o meu abrigo
+");
+
+Console.ResetColor();
+
+Console.WriteLine (@"[Ponte]
+Uh, uh, uh, uh
+Uh, uh
+
+[Verso 2]
+Quero presentear
+Com flores Iemanjá
+Pedir um paraíso
+Pra gente se encostar
+Uma viola a tocar
+Melodias pra gente dançar
+A benção das estrelas
+A nos iluminar
+
+[Pré-Refrão]
+Vida boa, brisa e paz
+Trocando olhares ao anoitecer
+Rir à toa é bom demais
+Olhar pro céu, sorrir e agradecer
+");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("[Refrão]");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"Você é a razão da minha felicidade
+Não vá dizer que eu não sou sua cara-metade
+Meu amor, por favor, vem viver comigo
+No seu colo é o meu abrigo
+");
+
+Console.ResetColor();
+
+Console.WriteLine(@"[Pós-Refrão]
+Meu abrigo
+No seu colo é o meu abrigo
+O meu abrigo
+
+[Ponte]
+Uh, uh, uh, ah
+Ah
+");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("[Refrão]");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"Você é a razão da minha felicidade
+Não vá dizer que eu não sou sua cara-metade
+Meu amor, por favor, vem viver comigo
+No seu colo é o meu abrigo
+");
+
+Console.ResetColor();
